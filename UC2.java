091379956b5a19b3e1d9d3c827beda3a2ef1b620
unknown <mr5285@srmist.edu.in>
2026-03-18 13:54:11 +0530

    UC2

--- UC2.java
+++ UC2.java
@@ -1,11 +1,9 @@
 public class UC2 {
-		public static void main(String[]args){
-			String name = "World";
-			
-			if(args.length > 0){
-				name = args[0];
-				}
-				
-				System.out.println("Hello, " + name + "!");
-				}
-				}
+public static void main(String[] args) {
+
+     String name = args[0];
+System.out.println("Hello, "+name+"!");
+
+}
+
+}
