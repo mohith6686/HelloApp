461dcdde8f7160dd9ef2622bfcd69233df61ef1e
unknown <mr5285@srmist.edu.in>
2026-04-13 13:17:37 +0530

    UC7

---
+++ UC7.java
@@ -0,0 +1,17 @@
+public class UC7 {
+
+    public static void main(String[] args) {
+
+        // If no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Join all arguments with ", "
+        String names = String.join(", ", args);
+
+        // Print greeting
+        System.out.println("Hello, " + names + "!");
+    }
+}
